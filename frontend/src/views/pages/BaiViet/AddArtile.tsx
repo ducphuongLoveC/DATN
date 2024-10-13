b80697b763f90b83c0b7c2358938a253b2ee805e
hanvph35034 <sonyeuthunhem@gmail.com>
2024-10-13 19:47:24 +0700

    giao diện bài viết

--- frontend/src/views/pages/BaiViet/AddArtile.tsx
+++ frontend/src/views/pages/BaiViet/AddArtile.tsx
@@ -4,20 +4,19 @@ import 'react-quill/dist/quill.snow.css';
 const AddArtile = () => {
   const [value, setValue] = useState('');
   return (
-   <>
-   <div className="p-4 bg-gray-100 rounded-lg shadow-md">
-  <h2 className="text-2xl font-bold mb-4 text-gray-800">Tiêu đề</h2>
-  <ReactQuill 
-    theme="snow" 
-    value={value} 
-    placeholder="Nội dung viết ở đây"
-    onChange={setValue} 
-    className="bg-white rounded-lg shadow-sm min-h-[500px] h-auto"
-  />
-</div>
-
+    <>
+      <div className="p-4 bg-gray-100 rounded-lg shadow-md">
+        <h2 className="text-2xl font-bold mb-4 text-gray-800">Tiêu đề</h2>
+        <ReactQuill
+          theme="snow"
+          value={value}
+          placeholder="Nội dung viết ở đây"
+          onChange={setValue}
+          className="bg-white rounded-lg shadow-sm min-h-[500px] h-auto"
+        />
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default AddArtile
+export default AddArtile;
--- frontend/src/views/pages/BaiViet/BannerArtile.tsx
+++ frontend/src/views/pages/BaiViet/BannerArtile.tsx
@@ -1,22 +1,27 @@
-import React from 'react'
+import React from 'react';
 
 const BannerArtile = () => {
   return (
     <div>
-        <section className="px-4">
+      <section className="px-4">
         <div
-    className="p-6 sm:p-10 rounded-2xl w-full text-white flex items-center justify-between max-w-2xl mx-auto mt-20 h-[180px] min-w-[1000px] bg-cover bg-center"
-    style={{ backgroundImage: "url('https://images.unsplash.com/photo-1511467687858-23d96c32e4ae?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w0NzEyNjZ8MHwxfHNlYXJjaHw1fHxrZXlib2FyZHxlbnwwfDB8fHwxNjk5NTI1MDAzfDA&ixlib=rb-4.0.3&q=80&w=1080')" }}>
-    <div className="flex flex-col gap-6">
-        <div className="">
-            <span className="text-gray-200 tw-justify-between text-4xl text-white font-semibold">Bài viết</span>
+          className="p-6 sm:p-10 rounded-2xl w-full text-white flex items-center justify-between max-w-2xl mx-auto mt-20 h-[180px] min-w-[1000px] bg-cover bg-center"
+          style={{
+            backgroundImage:
+              "url('https://images.unsplash.com/photo-1511467687858-23d96c32e4ae?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w0NzEyNjZ8MHwxfHNlYXJjaHw1fHxrZXlib2FyZHxlbnwwfDB8fHwxNjk5NTI1MDAzfDA&ixlib=rb-4.0.3&q=80&w=1080')",
+          }}
+        >
+          <div className="flex flex-col gap-6">
+            <div className="">
+              <span className="text-gray-200 tw-justify-between text-4xl text-white font-semibold">
+                Bài viết
+              </span>
+            </div>
+          </div>
         </div>
+      </section>
     </div>
-</div>
+  );
+};
 
-</section>
-    </div>
-  )
-}
-
-export default BannerArtile
+export default BannerArtile;
