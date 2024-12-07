aec00f2d581a3e41aa298d6c832d9138365ed63d
hanvph35034 <sonyeuthunhem@gmail.com>
2024-12-07 22:20:39 +0700

    fix

--- frontend/src/api/certificate.ts
+++ frontend/src/api/certificate.ts
@@ -1,13 +1,5 @@
 import axiosInstance from './axiosInstance';
 
-export const getCertificateByCertificateId = async (certificate_code: string) => {
-  try {
-    const { data } = await axiosInstance.get(`api/certificate/${certificate_code}`);
-    return data;
-  } catch (error) {
-    throw error;
-  }
-};
 
 export const createCertificate = async (payload: {}) => {
   console.log(payload);
